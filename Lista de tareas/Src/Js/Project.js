64450e42a0b8b15253c7cd0340b757bf408ec408
DylanMC14 <dmadrigalc@fwdcostarica.com>
2023-07-26 15:44:21 -0600

    Actualizacion de avance del proyecto

--- Lista de tareas/Src/Js/Project.js
+++ Lista de tareas/Src/Js/Project.js
@@ -2,36 +2,108 @@
 
 
 // --Variables-- \\
-let agrega= document.getElementById("boton1");
-let h5= document.querySelector(".pendientes");
+let agrega= document.getElementById("agrega");
+let h5= document.querySelector(".sintareas");
 const ContenedorTareas = document.getElementById('ContenedorTareas')
 let barra = document.getElementById("barr");
+var decrementador= document.getElementById ("resta");
+var aumentador= document.getElementById ("suma");
+let contador = 0;
+let contador2 = 0;
 // --Variables-- \\
 
 
 //----------------- Agregar tarea -----------------\\
-agrega.addEventListener ("click",function (e) {
-    e.preventDefault();
-    console.log(barra.value)
-    ContenedorTareas.innerHTML = barra.value
+const añadirtarea = event => {  
+event.preventDefault();
+const value =  barra.value
+console.log(value); 
+if( !value) { // --No ingrese un campo vacio o numeros solos-- \\
+    alert('Ingrese texto'); return;
+ }else if(!isNaN(value)) {
+    alert('No es un texto'); return; 
+ };
+//----------------- ================ -----------------\\
+                        // && \\
+//----------------- ================ -----------------\\
+ const tarea= document.createElement("li");  // --Crea una lista desordenada-- \\
 
-    const tareas = [];
-    tareas.push (barra.value);
-    // h5.textContent=ContenedorTareas
-    // console.log (tareas);
-});
+tarea.addEventListener("click",tareanueva)  // --Agrega la tarea-- \\
+    tarea.textContent= value
+    ContenedorTareas.appendChild(tarea);
+    event.target.reset();
+//----------------- ================ -----------------\\
+                        // && \\
+//----------------- ================ -----------------\\
+    var eliminar = document.createElement("span"); // --Eliminar tarea-- \\
+        eliminar.textContent="❌";
+        tarea.appendChild(eliminar);
+
+        incrementar1();
+        Actualizar1();
+//----------------- ================ -----------------\\
+                       // && \\
+//----------------- ================ -----------------\\
+        var check = document.createElement("span"); // --Tarea completa-- \\
+        check.textContent="✅";
+        tarea.prepend(check);
+
+        Actualizar2();
+//----------------- ================ -----------------\\
+                      // && \\
+//----------------- ================ -----------------\\
+        check.addEventListener ("click",function(eliminartareas){
+            tarea.remove();  // --Funcion de check-- \\
+
+            Decrementar1();
+            incrementar2();
+            Actualizar2();
+            Actualizar1();
+        });
+//----------------- ================ -----------------\\
+                      // && \\
+//----------------- ================ -----------------\\
+        eliminar.addEventListener ("click",function(eliminartareas){
+            tarea.remove();  // --Funcion de eliminar-- \\
+            
+            Decrementar1();
+            Actualizar1();
+        });
+    };
+//----------------- ================ -----------------\\
+                      // && \\
+//----------------- ================ -----------------\\
+    tareanueva= event => {
+        event.target.toggle("done");     // --Funcion para agregar tarea-- \\
+    };
+//----------------- ================ -----------------\\
+                      // && \\
+//----------------- ================ -----------------\\
+function incrementar1(){ 
+    contador++
+};
+function Decrementar1(){      // --Funcion de contador1-- \\
+    contador--
+};
+function Actualizar1(){
+    decrementador.textContent = contador
+};
+//----------------- ================ -----------------\\
+                      // && \\
+//----------------- ================ -----------------\\
+function incrementar2(){
+    contador2++
+};                            // --Funcion de contador2-- \\
+
+function Actualizar2(){
+    aumentador.textContent = contador2
+};
+//----------------- ================ -----------------\\
+                      // && \\
+//----------------- ================ -----------------\\
+
+//----------------- TEXTO: "No Hay Tareas" -----------------\\
 
 
-
-//----------------- Contador -----------------\\
-var sumas = document.getElementById("suma"); //------- Suma por tarea completada
-var circle = 0;
-
-boton1.addEventListener ("click",function () {
-    circle++
-    sumas.innerHTML = circle  
-});
-
-//----------------- Eliminar tarea -----------------\\
 
 // ---------------------------------------------------------------------------------------- \\
